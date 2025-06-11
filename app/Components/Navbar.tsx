8e3d9fc54874e5a8a98fbc5928cec65e4fdccbb3
Merilyne <nchiamerilyne@gmail.com>
2025-06-11 10:40:50 -0600

    added page.js

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,8 @@
+// components/Navbar.tsx
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white px-6 py-4 shadow">
+      <h1 className="text-2xl font-bold">New Generation High School Portal</h1>
+    </nav>
+  );
+}
